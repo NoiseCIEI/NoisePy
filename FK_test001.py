20c4f29d666ffcecb28d39affdb82dece85de38d
Lili Feng <lili.feng@colorado.edu>
2016-04-08 16:59:38 -0600

    add fk data with large Q

--- FK_test001.py
+++ FK_test001.py
@@ -16,7 +16,7 @@ evla=0.0;
 evlo=0.0;
 
 fkStream=obspy.Stream();
-fkdatadir='/projects/life9360/code/fk/ak135_15';
+fkdatadir='/projects/life9360/code/fk/ak135_Q_15';
 for sacfname in glob.glob(fkdatadir+"/*0"):
     Tr=obspy.read(sacfname)[0];
     Tr.stats.distance=Tr.stats.sac.dist;
@@ -26,7 +26,7 @@ DeltaArr=np.array([]);
 VgrArr=np.array([]);
 AmpArr=np.array([]);
 DistArr=np.array([]);
-per=12.;
+per=15.;
 for trace in fkStream:
     nTr=noisepy.noisetrace(trace.data, trace.stats);
     # nTr=symData2d.symtrace(trace.data, trace.stats);
@@ -62,11 +62,14 @@ for trace in fkStream:
     # AmpArr=np.append(AmpArr, nTr.SNRParam.amp_s[0]);
     AmpArr=np.append(AmpArr, Amp);
     
-    
+minindex=np.argmin(DistArr)
 plt.figure();
 # plt.plot(DistArr, DistArr/VgrArr, 'o');
 # plt.plot(DistArr, VgrArr, 'o');
-plt.plot(DistArr, (VgrArr-VgrArr[0])/VgrArr[0]*100.,'o' );
+plt.plot(DistArr, (VgrArr-VgrArr[minindex])/VgrArr[minindex]*100.,'o' );
+plt.title('original aftan')
+# plt.title('modified aftan')
+
 plt.ylabel('Relative Difference in Vgr (%)');
 # plt.ylabel('Vgr(km/s)');
 plt.xlabel('Distance(km)');
@@ -75,25 +78,32 @@ plt.figure();
 # plt.plot(DistArr, VgrArr, 'x');
 # plt.plot(DistArr, AmpArr*np.sqrt(np.sin(DeltaArr*np.pi/180.) ));
 plt.plot(DistArr, AmpArr*1e9,'o' );
+plt.title('original aftan')
+# plt.title('modified aftan')
 plt.ylabel('Amplitude(nm)');
 plt.xlabel('Distance(km)');
 
 plt.figure();
 # plt.plot(DistArr, VgrArr, 'x');
 # plt.plot(DistArr, AmpArr*np.sqrt(np.sin(DeltaArr*np.pi/180.) ));
-# CampArr=AmpArr*np.sqrt( np.sin(DeltaArr*np.pi/180.) ) /np.sqrt(np.sin(DeltaArr[0]*np.pi/180.) )
-# CampArr=AmpArr*np.sqrt(DistArr/DistArr[0] )  
-CampArr=AmpArr*DistArr/ DistArr[0]
+# CampArr=AmpArr*np.sqrt( np.sin(DeltaArr*np.pi/180.) ) /np.sqrt(np.sin(DeltaArr[minindex]*np.pi/180.) )
+# CampArr=AmpArr*np.sqrt(DistArr/DistArr[minindex] )
+# CampArr=AmpArr*np.sin(DeltaArr*np.pi/180.)  /np.sin(DeltaArr[0]*np.pi/180.) 
+CampArr=AmpArr*DistArr/ DistArr[minindex]
 # CampArr=AmpArr*(DistArr/ DistArr[0])**(0.8)
-plt.plot(DistArr, (CampArr-CampArr[0])/CampArr[0]*100.,'o' );
+plt.plot(DistArr, (CampArr-CampArr[minindex])/CampArr[minindex]*100.,'o' );
 # plt.plot(DistArr, CampArr,'o' );
-# plt.
+plt.title('original aftan')
+# plt.title('modified aftan')
 plt.ylabel('Relative Difference in Corrected Amp (%)');
 plt.xlabel('Distance(km)');
+
+
 # plt.axis([ DistArr.min(), DistArr.max(), CampArr.min(), CampArr.max()])
 plt.show()
-slope, intercept, r_value, p_value, std_err = stats.linregress(DistArr, DistArr/VgrArr);
+# slope, intercept, r_value, p_value, std_err = stats.linregress(DistArr, DistArr/VgrArr);
+slope, intercept, r_value, p_value, std_err = stats.linregress(DistArr, CampArr*1e9);
 print slope, intercept, r_value, p_value, std_err
     
     
-fkStream.plot(type='section', norm_method='stream', alpha=1)#     
+fkStream.plot(type='section',  alpha=1)#     
